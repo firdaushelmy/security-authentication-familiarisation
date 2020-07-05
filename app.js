71b409157bd4582cd03e055df4fc9ab79a247393
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-07-06 02:23:52 +0800

    add get routing for home, login, and register page

--- app.js
+++ app.js
@@ -10,10 +10,25 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const port = process.env.PORT || 3000;
 // const MONGO_URL = process.env.MONGO_URL
 // mongoose.connect(`${MONGO_URL}`, { useNewUrlParser: true, useUnifiedTopology: true })
+
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+app.get('/login', function (req, res) {
+  res.render('login');
+});
+
+app.get('/register', function (req, res) {
+  res.render('register');
+});
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
   console.log(`server is running on ${port}`)
 });
+
