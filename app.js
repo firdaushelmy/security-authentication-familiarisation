4a75fa27727fe14579003bba78cd946ecf99be8e
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-07-07 19:19:01 +0800

    add new mongoose schema and model + post for registration with new user.save

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ app.use(express.static('public'));
 const MONGO_URL = process.env.MONGO_URL
 mongoose.connect(`${MONGO_URL}`, { useNewUrlParser: true, useUnifiedTopology: true })
 
+const userSchema = {
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+};
+
+const User = new mongoose.model('User', userSchema);
 
 app.get('/', function (req, res) {
   res.render('home');
@@ -25,6 +31,21 @@ app.get('/login', function (req, res) {
 app.get('/register', function (req, res) {
   res.render('register');
 });
+
+app.post('/register', function (req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser.save(function (err) {
+    if (!err) {
+      res.render('secrets')
+      return;
+    };
+    console.log(err)
+  });
+})
 
 const port = process.env.PORT || 3000;
 
