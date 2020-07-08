75ffadf6cfdc9c2979b33a5f1d466f5c96a5718d
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-07-08 21:36:33 +0800

    remove bcrypt + init in session, passport, passportLocalMongoose

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -36,46 +38,11 @@ app.get('/register', function (req, res) {
 
 app.post('/register', function (req, res) {
 
-  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash,
-    });
-    newUser.save(function (err) {
-      if (!err) {
-        res.render('secrets')
-        return;
-      };
-      console.log(err)
-    });
-  });
 
 })
 
 app.post('/login', function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
 
-  User.findOne({ email: username }, function (err, foundUser) {
-    if (err) {
-      console.log('error: ' + err);
-      return;
-    }
-    if (!foundUser) {
-      console.log('user does not exist');
-    }
-    if (foundUser) {
-      bcrypt.compare(password, foundUser.password, function (err, result) {
-        if (result === false) {
-          console.log('email or password does not match');
-          return;
-        }
-        if (result === true) {
-          res.render('secrets');
-        }
-      })
-    }
-  })
 })
 
 
