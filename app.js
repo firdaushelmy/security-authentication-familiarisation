8252f99675f48cf6a15b1fe021d2236efeeae9d5
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-07-10 18:41:50 +0800

    add login page

--- app.js
+++ app.js
@@ -76,7 +76,21 @@ app.post('/register', function (req, res) {
 
 app.post('/login', function (req, res) {
 
-})
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err)
+    } else {
+      passport.authenticate('local')(req, res, function () {
+        res.redirect('/secrets');
+      });
+    }
+  });
+});
 
 
 const port = process.env.PORT || 3000;
