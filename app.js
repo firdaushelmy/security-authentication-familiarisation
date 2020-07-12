e0fbb4bcbe2ab2425850508215914825d5e9441b
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-07-13 01:45:02 +0800

    add facebook login + add facebook social button on register and login ejs

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
 
 
@@ -34,6 +35,7 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
   googleId: String,
+  facebookId: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -65,6 +67,18 @@ passport.use(new GoogleStrategy({
   }
 ));
 
+passport.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_CLIENT_ID,
+  clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+  callbackURL: 'http://localhost:3000/auth/facebook/secrets'
+},
+  function (accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 app.get('/', function (req, res) {
   res.render('home');
 });
@@ -78,6 +92,16 @@ app.get('/auth/google/secrets',
   function (req, res) {
     res.redirect('/secrets');
   });
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook')
+);
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: 'login' }),
+  function (req, res) {
+    res.redirect('/secrets');
+  })
 
 app.get('/login', function (req, res) {
   res.render('login');
